a4c148fa55df1d0d075dab695937ca29f201f147
Eraserstreet <148534901+Eraserstreet@users.noreply.github.com>
2024-04-12 19:33:56 +0300

    JS accordion
    не працює підключення

---
+++ src/js/faq.js
@@ -0,0 +1,28 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const titles = document.querySelectorAll('.faq-title');
+const contents = document.querySelectorAll('.accordion-open');
+
+titles.forEach(item => {
+  item.addEventListener('click', () => {
+    const activeContent = document.querySelector(
+      '#' + item.getAttribute('data-tab')
+    );
+
+    if (activeContent.classList.contains('active')) {
+      activeContent.classList.remove('active');
+      activeContent.style.maxHeight = 0;
+    } else {
+      contents.forEach(element => {
+        element.classList.remove('active');
+        element.style.maxHeight = 0;
+      });
+      titles.forEach(element => element.classList.remove('active'));
+
+      item.classList.add('active');
+      activeContent.classList.add('active');
+      activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+    }
+  });
+});
