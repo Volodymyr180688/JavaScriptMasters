e36db86b05a01e6191d57c62c0daf3eeff1ab017
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-04-12 15:31:25 +0300

    add

---
+++ src/js/about-me.js
@@ -0,0 +1,16 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+new Accordion('.about-me-list', {
+  duration: 400,
+  showMultiple: true,
+});
+
+const aboutSwiper= new Swiper('.about-me-swiper', {
+    speed: 400,
+    spaceBetween: 100,
+  });
+const aboutMeSwiper = document.querySelector('.about-me-swiper').swiper;
+aboutMeSwiper();
