367fda9e44c0365d6d8806dcc4bc3534da4e9407
Violetta Velykotrav <violettavelykotrav@gmail.com>
2024-04-13 03:11:12 +0300

    add styles for projects section

---
+++ src/js/projects.js
@@ -0,0 +1,13 @@
+// import Swiper from 'swiper';
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   new Swiper('.swiper', {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
+// });
