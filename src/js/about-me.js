e1bfe6d5c4dd195732904485e2df5be4bf8aa264
Volodymyr Shorobura <147277754+Volodymyr180688@users.noreply.github.com>
2024-04-14 21:07:33 +0300

    Update about-me.js

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,25 +1,25 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-const titles = document.querySelectorAll('.about-me-title');
-const contents = document.querySelectorAll('.accordion-open-about-me');
-titles.forEach(item => {
-  item.addEventListener('click', () => {
-    const activeContent = document.querySelector(
-      '#' + item.getAttribute('data-tab')
-    );
-    if (activeContent.classList.contains('active')) {
-      activeContent.classList.remove('active');
-      activeContent.classList.add('accordion-open');
-      activeContent.style.maxHeight = 0;
-    } else {
-      contents.forEach(element => {
-        element.classList.remove('active');
-        element.style.maxHeight = 0;
-      });
-      titles.forEach(element => element.classList.remove('active'));
-      item.classList.add('active');
-      activeContent.classList.add('active');
-      activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
-    }
-  });
-});
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
+// const titles = document.querySelectorAll('.about-me-title');
+// const contents = document.querySelectorAll('.accordion-open-about-me');
+// titles.forEach(item => {
+//   item.addEventListener('click', () => {
+//     const activeContent = document.querySelector(
+//       '#' + item.getAttribute('data-tab')
+//     );
+//     if (activeContent.classList.contains('active')) {
+//       activeContent.classList.remove('active');
+//       activeContent.classList.add('accordion-open');
+//       activeContent.style.maxHeight = 0;
+//     } else {
+//       contents.forEach(element => {
+//         element.classList.remove('active');
+//         element.style.maxHeight = 0;
+//       });
+//       titles.forEach(element => element.classList.remove('active'));
+//       item.classList.add('active');
+//       activeContent.classList.add('active');
+//       activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+//     }
+//   });
+// });
