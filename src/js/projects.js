c24b9a1a9d67726f07900826a2bd83bee211398d
Violetta Velykotrav <violettavelykotrav@gmail.com>
2024-04-14 14:24:29 +0300

    add styles for buttons

--- src/js/projects.js
+++ src/js/projects.js
@@ -15,5 +15,30 @@ const swiper = new Swiper('.swiper', {
   centeredSlides: true,
   slidesPerView: 'auto',
   autoHeight: true,
-
+  // on: {
+  //   reachEnd: function () {
+  //     const nextButton = document.querySelector('.next-btn');
+  //     nextButton.disabled = true;
+  //   },
+  //     reachBeginning: function () {
+  //     const nextButton = document.querySelector('.next-btn');
+  //     nextButton.disabled = false;
+  //   },
+  // },
+on: {
+    slideChangeTransitionEnd: function () {
+      const nextButton = document.querySelector('.next-btn');
+      const prevButton = document.querySelector('.prev-btn');
+      if (swiper.isEnd) {
+        nextButton.disabled = true;
+      } else {
+        nextButton.disabled = false;
+      }
+      if (swiper.isBeginning) {
+        prevButton.disabled = true;
+      } else {
+        prevButton.disabled = false;
+      }
+    },
+  },
 });
