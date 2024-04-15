f50df0f05a45352d44ec78abccc0f737382c39ee
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-04-15 14:10:43 +0300

    about-me-swiper

--- src/js/swiper-about.js
+++ src/js/swiper-about.js
@@ -1,8 +1,57 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+// import { Navigation, Pagination, Keyboard } from 'swiper/modules';
+import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+
+// const aboutMeSwiperBox = document.querySelector('.swiper-about-me-list');
+// const aboutMeNextButton = document.querySelector('.swiper-button-next-about-me');
+
+// const swiper = new Swiper(".aboutMeSwiperBox", {
+//   cssMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next-about-me",
+//     prevEl: ".swiper-button-prev-about-me",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
 
 
 
+// const aboutMeSwiperBox = document.querySelector('#wiper-about-me-list');
+// const aboutMeNextButton = document.querySelector('.wiper-button-next-about-me');
 
-const swiperAboutMe = document.querySelector('#wiper-about-me-list');
-const buttonEl = document.querySelector('.wiper-button-next-about-me');
+// const swiper = new swiper(aboutMeSwiperBox, {
+//   modules: [Navigation, Keyboard],
+//   navigation: {
+//     nextEl: aboutMeNextButton,
+//   },
+//   loop: true,
+//   direction: 'horizontal',
+//   watchOverflow: true,
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+//     pageUpDown: true,
+//   },
+
+//   breakpoints: {
+//     375: {
+//       slidesPerView: 2,
+//     },
+//     768: {
+//       slidesPerView: 3,
+//     },
+//     1440: {
+//       slidesPerView: 'auto',
+//     },
+//   },
+
+ 
+// });
+
