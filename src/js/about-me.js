ce7312bd140f562889d9c0746b34011ce20517a2
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-04-15 11:52:35 +0300

    add

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,22 +1,15 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
-
-
-
-
 const titles = document.querySelectorAll('.about-me-title');
 const contents = document.querySelectorAll('.accordion-open-about-me');
-
-
 titles.forEach(item => {
   item.addEventListener('click', () => {
     const activeContent = document.querySelector(
       '#' + item.getAttribute('data-tab')
     );
-
     if (activeContent.classList.contains('active')) {
       activeContent.classList.remove('active');
-      activeContent.classList.add('accordion-open');
+      activeContent.classList.add('accordion-open-about-me');
       activeContent.style.maxHeight = 0;
     } else {
       contents.forEach(element => {
@@ -24,7 +17,6 @@ titles.forEach(item => {
         element.style.maxHeight = 0;
       });
       titles.forEach(element => element.classList.remove('active'));
-
       item.classList.add('active');
       activeContent.classList.add('active');
       activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
